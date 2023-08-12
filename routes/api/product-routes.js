0d994f6d74fbf84977a12a81b80ee3f92755e7a1
JayRodButray <jayrodbutray@gmail.com>
2023-08-11 22:15:22 -0700

    pnpm install,change.env,fix product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,36 +41,26 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
 
-    try {
-      const productData = await Product.create(req.body);
-      res.status(200).json(productData);
-    } catch (err) {
-      res.status(400).json(err);
+    // if there's product tags, create pairings in the ProductTag model
+    if (req.body.tagIds && req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+      await ProductTag.bulkCreate(productTagIdArr);
     }
-  });
 
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-
+    res.status(201).json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
 
 // update product
 router.put('/:id', (req, res) => {
